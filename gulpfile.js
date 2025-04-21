0f6f6f5aec768cf47c6691d5d5633f5cc10ffb4c
CR1ST1AN200 <cfmart1n200@gmail.com>
2025-04-20 23:51:41 -0300

    Actualización de gulpfile y package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,54 @@
 'use strict';
 
 const fs = require('fs');
-const gulp = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
 const packager = require('electron-packager');
 
 const config = JSON.parse(fs.readFileSync('package.json'));
 const appVersion = config.version;
 const electronVersion = config.devDependencies['electron'].match(/[\d.]+/)[0];
 const options = {
-	asar: true,
-	dir: '.',
-	icon: './app/img/markdownify.icns',
-	name: 'Markdownify',
-	out: 'dist',
-	overwrite: true,
-	prune: true,
-	version: electronVersion,
-	'app-version': appVersion
+    asar: true,
+    dir: '.',
+    icon: './app/img/markdownify.icns',
+    name: 'Markdownify',
+    out: 'dist',
+    overwrite: true,
+    prune: true,
+    version: electronVersion,
+    'app-version': appVersion
 };
 
-gulp.task('build:osx', (done) => {
-	options.arch = 'x64';
-	options.platform = 'darwin';
-	options['app-bundle-id'] = 'com.amitmerchant.markdownify';
-	options['helper-bundle-id'] = 'com.amitmerchant.markdownify.helper';
+function buildOSX(done) {
+    options.arch = 'x64';
+    options.platform = 'darwin';
+    options['app-bundle-id'] = 'com.amitmerchant.markdownify';
+    options['helper-bundle-id'] = 'com.amitmerchant.markdownify.helper';
 
-	packager(options, (err, paths) => {
-		if (err) {
-			console.error(err);
-		}
+    packager(options, (err, paths) => {
+        if (err) {
+            console.error(err);
+        }
+        done();
+    });
+}
 
-		done();
-	});
-});
+function buildLinux(done) {
+    // @TODO: Implementar la lógica para Linux si es necesario
+    done();
+}
 
-gulp.task('build:linux', () => {
-	// @TODO
-});
+function buildWindows(done) {
+    options.arch = 'x64';
+    options.platform = 'win32';
 
-gulp.task('build:windows', () => {
-	options.arch = 'x64';
-	options.platform = 'win32';
-	packager(options, (err, paths) => {
-		if (err) {
-			console.error(err);
-		}
+    packager(options, (err, paths) => {
+        if (err) {
+            console.error(err);
+        }
+        done();
+    });
+}
 
-		done();
-	});
-});
-
-gulp.task('build', ['build:osx', 'build:linux', 'build:windows']);
+// ✅ Definir la tarea `build` correctamente en Gulp 4
+exports.build = parallel(buildOSX, buildLinux, buildWindows);
