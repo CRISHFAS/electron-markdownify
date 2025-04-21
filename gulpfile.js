8210f724fe70b24e0af3be81c2492d6de67b5c40
CR1ST1AN200 <cfmart1n200@gmail.com>
2025-04-21 00:57:14 -0300

    Ajustes en gulpfile.js para Netlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,12 @@
 'use strict';
 
-const fs = require('fs');
-const { src, dest, series, parallel } = require('gulp');
-const packager = require('electron-packager');
+const { src, dest } = require('gulp');
 
-const config = JSON.parse(fs.readFileSync('package.json'));
-const appVersion = config.version;
-const electronVersion = config.devDependencies['electron'].match(/[\d.]+/)[0];
-const options = {
-    asar: true,
-    dir: '.',
-    icon: './app/img/markdownify.icns',
-    name: 'Markdownify',
-    out: 'dist',
-    overwrite: true,
-    prune: true,
-    version: electronVersion,
-    'app-version': appVersion
-};
-
-function buildOSX(done) {
-    options.arch = 'x64';
-    options.platform = 'darwin';
-    options['app-bundle-id'] = 'com.amitmerchant.markdownify';
-    options['helper-bundle-id'] = 'com.amitmerchant.markdownify.helper';
-
-    packager(options, (err, paths) => {
-        if (err) {
-            console.error(err);
-        }
-        done();
-    });
+function copyWebFiles() {
+    return src(['app/**/*.html', 'app/**/*.css', 'app/**/*.js'])
+        .pipe(dest('dist'));
 }
 
-function buildLinux(done) {
-    // @TODO: Implementar la lógica para Linux si es necesario
-    done();
-}
+// ✅ Solo copiar archivos web estáticos
+exports.build = copyWebFiles;
 
-function buildWindows(done) {
-    options.arch = 'x64';
-    options.platform = 'win32';
-
-    packager(options, (err, paths) => {
-        if (err) {
-            console.error(err);
-        }
-        done();
-    });
-}
-
-// ✅ Definir la tarea `build` correctamente en Gulp 4
-exports.build = parallel(buildOSX, buildLinux, buildWindows);
